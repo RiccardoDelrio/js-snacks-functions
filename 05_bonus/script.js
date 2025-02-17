e9bfe10ab26f1e086616a3fd7a705e9b7478a987
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-17 17:07:06 +0100

    add function to return a different greeting based on the time

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,11 +5,21 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const nome = 'Mario';
+//ho cambiato il nome della variabile perche risultava"deprecated"
 
 // Dichiara la funzione qui.
+const sayHello = (str) => {
+    let now = new Date().getHours()
+    if (now <= 13) {
+        console.log(`"Buongiorno " ${str}`);
+    } else if (now <= 17) {
+        console.log(`"Buon pomeriggio " ${str}`);
+    } else
+        console.log(`"Buonasera " ${str}`);
 
+}
+sayHello(nome)
 
 // Invoca la funzione qui e stampa il risultato in console
 
