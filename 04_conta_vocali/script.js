1665235b2d67b23c2c3ab558e2a8ec1586a866dd
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-17 16:54:42 +0100

    add function to count the vowels present in a string

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const vocalCount = (str) => {
+    const vocali = ["a", "e", "i", "o", "u"]
+    let count = 0
+    let vocalitrovate = []
+    for (let i = 0; i < str.length; i++) {
+        if (vocali.includes(str[i])) {
+            count++
+            vocalitrovate.push(str[i])
+        }
+
+    }
+    return count, vocalitrovate
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(vocalCount(word));
 
 
 
