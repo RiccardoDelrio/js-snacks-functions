2bf3d059dfe031869e14404aec828d843712dc0c
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-17 16:11:39 +0100

    Created function for returning a greeting

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,9 +5,9 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
-
+const sayHello = (word) => "Ciao " + word
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(sayHello(userName))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
