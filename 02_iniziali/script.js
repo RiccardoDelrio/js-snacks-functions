a5366d1e0a0fdeb41e9083f15bec3bf38d74b2bd
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-17 16:30:30 +0100

    add function to find the first letter of a word in an array

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+const firstLetter = (arr) => {
+    const letters = []
+    for (let i = 0; i < arr.length; i++) {
+        let lettera = arr[i].charAt(0)
+        letters.push(lettera);
+    }
 
+    return letters
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(firstLetter(names))
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
